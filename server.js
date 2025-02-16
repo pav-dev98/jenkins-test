87b99bf9548253971f132e321aa58b7f0f3becc2
pavel mansilla goyas <pavel.mansilla@gmail.com>
2025-02-16 00:54:32 -0500

    new push event

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Jenkins');
+    res.send('Hello Jenkins with push event');
 });
 
 app.listen(PORT, () => {
